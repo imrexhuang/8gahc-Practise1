3589da472156d38df37c45e20205d8f9b4f770ab
Rex <im@rex.tw>
2018-07-18 16:50:28 +0800

    public class ModelContext : DbContext

---
+++ InsuranceSys/Models/ModelContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+
+namespace InsuranceSys.Models
+{
+    public class ModelContext : DbContext
+    {
+        public ModelContext() : base("name=DBEntities")
+        {
+        }
+
+        public virtual DbSet<Customer> Customer { get; set; }
+        public virtual DbSet<CustomerDetail> CustomerDetail { get; set; }
+        public virtual DbSet<UserInfo> UserInfo { get; set; }
+        public virtual DbSet<SysCode> SysCode { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
